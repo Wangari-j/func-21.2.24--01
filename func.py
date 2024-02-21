3783fd8534826059a90f4b0776d9426878c4675a
Wangari-j <wangarijudy@gmail.com>
2024-02-21 11:59:37 +0300

    functions

---
+++ func.py
@@ -0,0 +1,11 @@
+def sumNum(num1,num2):
+
+    if num1 == num2:
+        return("True")
+    else:
+        return("False")
+    
+    
+print(sumNum(4, 8))
+print(sumNum(2, 2))
+print(sumNum(2, "2"))
